c2d78c2563c30983a4985d79e06b4c6a96a76305
Mohd-Tanveer-Alam <64190001+Mohd-Tanveer-Alam@users.noreply.github.com>
2020-12-06 19:54:36 +0530

    Add files via upload

---
+++ New Vision Export/contact-form-handler.php
@@ -0,0 +1,18 @@
+<?php
+$name =$_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'info@newvisionexports.com';
+
+$email_subject = "New Form Submittion";
+
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "User Message: $message.\n";
+$to = "tanveer.akon15@gmail.com";
+$headers = "From: $email_form \r\n";
+$headers = "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: index.html");
+?>
